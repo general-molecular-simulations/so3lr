20c9aae7c39e5bd6e93f53a97b189698bf4beb0f
Thorben Frank <thorbenjan.frank@gmail.com>
2024-08-14 18:54:24 +0200

    tests for jax md energy and force fn

---
+++ sup_gems/jaxmd_utils.py
@@ -0,0 +1,125 @@
+import jax
+import jax.numpy as jnp
+
+from collections import namedtuple
+from functools import partial
+from jax_md import partition
+from jax_md.space import DisplacementOrMetricFn, Box
+
+Graph = namedtuple(
+    "Graph",
+    (
+        "positions",
+        "edges",
+        "nodes",
+        "centers",
+        "others",
+        "mask",
+        "total_charge",
+        "num_unpaired_electrons",
+        "edges_lr",
+        "idx_i_lr",
+        "idx_j_lr",
+        "cell"
+    )
+)
+
+
+def neighbor_list_featurizer(displacement_fn, species):
+    def featurize(R, neighbor, neighbor_lr, **kwargs):
+        idx_i = neighbor[0]  # shape: P
+        idx_j = neighbor[1]  # shape: P
+        idx_i_lr = neighbor_lr[0]  # shape: P
+        idx_j_lr = neighbor_lr[1]  # shape: P
+
+        Ra = R[idx_i]
+        Rb = R[idx_j]
+        Ra_lr = R[idx_i_lr]
+        Rb_lr = R[idx_j_lr]
+
+        if 'box' in kwargs:
+            box = kwargs.get('box')
+
+        d = jax.vmap(partial(displacement_fn, **kwargs))
+        dR = d(Ra, Rb)
+        dR_lr = d(Ra_lr, Rb_lr)
+
+        graph = Graph(
+            positions=None,
+            nodes=species,
+            edges=dR,
+            centers=idx_i,
+            others=idx_j,
+            mask=None,
+            total_charge=jnp.array([0.]),
+            num_unpaired_electrons=jnp.array([0.]),
+            edges_lr=dR_lr,
+            idx_i_lr=idx_i_lr,
+            idx_j_lr=idx_j_lr,
+            cell=box  # will raise an error if box not in kwargs.
+        )
+
+        return graph
+
+    return featurize
+
+
+def to_jax_md(
+        potential,  # the mlff potential
+        displacement_or_metric: DisplacementOrMetricFn,
+        box_size: Box,  # box if it exists, check md jax documentation for conventions
+        species: jnp.ndarray = None,  # the atomic species, jnp.ndarray of shape n_atoms
+        dr_threshold: float = 0.,  # currently dr_threshold > 0 is experimental
+        capacity_multiplier: float = 1.25,
+        buffer_size_multiplier_sr: float = 1.25,
+        buffer_size_multiplier_lr: float = 1.25,
+        minimum_cell_size_multiplier_sr: float = 1.0,
+        minimum_cell_size_multiplier_lr: float = 1.0,
+        disable_cell_list: bool = False,
+        fractional_coordinates: bool = True,
+        **neighbor_kwargs
+):
+    # create the neighbor_fn
+    neighbor_fn = partition.neighbor_list(
+        displacement_or_metric,
+        box_size,
+        potential.cutoff,  # load the cutoff of the model from the MLFFPotential
+        dr_threshold,
+        capacity_multiplier,
+        buffer_size_multiplier_sr,  # as buffer_size_multiplier
+        minimum_cell_size_multiplier_sr,
+        fractional_coordinates=fractional_coordinates,
+        format=partition.NeighborListFormat(1),  # only sparse is supported in mlff
+        disable_cell_list=disable_cell_list,
+        **neighbor_kwargs)
+
+    # create the neighbor_fn for long-range cutoff
+    neighbor_fn_lr = partition.neighbor_list(
+        displacement_or_metric,
+        box_size,
+        potential.long_range_cutoff,
+        dr_threshold,
+        capacity_multiplier,
+        buffer_size_multiplier_lr,  # as buffer_size_multiplier
+        minimum_cell_size_multiplier_lr,
+        fractional_coordinates=fractional_coordinates,
+        format=partition.NeighborListFormat(2),  # long-range modules can handle OrderedSparse.
+        disable_cell_list=disable_cell_list,
+        **neighbor_kwargs)
+
+    featurizer = neighbor_list_featurizer(
+        displacement_or_metric,
+        species
+    )
+
+    # create an energy_fn that is compatible with jax_md
+    def energy_fn(
+            R,
+            neighbor,
+            neighbor_lr,
+            **energy_fn_kwargs
+    ):
+        graph = featurizer(R, neighbor, neighbor_lr, **energy_fn_kwargs)
+        return potential(graph).sum()
+
+    return neighbor_fn, neighbor_fn_lr, energy_fn
---
+++ tests/test_jaxmd.py
@@ -0,0 +1,252 @@
+import jax
+import jax.numpy as jnp
+import numpy as np
+import numpy.testing as npt
+import pathlib
+import pytest
+
+from ase.io import read
+from jax_md import space, quantity
+from mlff.mdx.potential import MLFFPotentialSparse
+from sup_gems import jaxmd_utils
+
+lr_cutoff = 12.0
+lr_cutoff_damp = 2.0
+
+capacity_multiplier = 1.25
+buffer_size_multiplier_sr = 1.25
+buffer_size_multiplier_lr = 1.25
+minimum_cell_size_multiplier_sr = 1.0
+
+
+@pytest.mark.parametrize('name', ['atat', 'dha', 'bb'])
+def test_molecules_with_box(name: str):
+    jax.config.update('jax_enable_x64', True)
+
+    package_dir = pathlib.Path(__file__).parent.parent.resolve()
+
+    # targets
+    target_predictions = np.load(package_dir / f'tests/test_data/{name}_jaxmd.npz')
+
+    # read molecule
+    mol = read(
+        package_dir / f'tests/test_data/{name}.xyz'
+    )
+
+    mol.set_cell([999.999, 999.999, 999.999])
+    current_center = mol.get_center_of_mass()
+    desired_center = mol.get_cell().sum(axis=0) / 2
+    translation_vector = desired_center - current_center
+    mol.translate(translation_vector)
+
+    species = jnp.array(mol.get_atomic_numbers())
+    atomic_masses = jnp.array(mol.get_masses())
+    box = jnp.array([999.999, 999.999, 999.999])
+    R0 = jnp.array(mol.get_positions()) / box[0]
+
+    # define mlff potential
+    mlff_potential = MLFFPotentialSparse.create_from_ckpt_dir(
+        package_dir / 'sup_gems' / 'sup_gems_params',
+        from_file=True,
+        long_range_kwargs=dict(
+            cutoff_lr=lr_cutoff,  # this controls whether elec and disp modules use force shifting and damping.
+            dispersion_energy_cutoff_lr_damping=lr_cutoff_damp,
+            neighborlist_format_lr='ordered_sparse'
+        ),
+        dtype=jnp.float64
+    )
+
+    displacement, shift = space.periodic_general(
+        box,
+        fractional_coordinates=True
+    )
+
+    neighbor_fn, neighbor_fn_lr, energy_fn = jaxmd_utils.to_jax_md(
+        potential=mlff_potential,
+        displacement_or_metric=displacement,
+        box_size=box,
+        species=species,
+        capacity_multiplier=capacity_multiplier,
+        buffer_size_multiplier_sr=buffer_size_multiplier_sr,
+        buffer_size_multiplier_lr=buffer_size_multiplier_lr,
+        minimum_cell_size_multiplier_sr=minimum_cell_size_multiplier_sr,
+        disable_cell_list=True
+    )
+
+    energy_fn = jax.jit(energy_fn)
+    force_fn = jax.jit(quantity.force(energy_fn))
+
+    nbrs = neighbor_fn.allocate(R0, box=box)
+    nbrs_lr = neighbor_fn_lr.allocate(R0, box=box)
+
+    energy = energy_fn(R0, neighbor=nbrs.idx, neighbor_lr=nbrs_lr.idx, box=box)
+    forces = force_fn(R0, neighbor=nbrs.idx, neighbor_lr=nbrs_lr.idx, box=box)
+
+    npt.assert_allclose(
+        target_predictions['energy'],
+        energy,
+        atol=1e-6
+    )
+
+    npt.assert_allclose(
+        target_predictions['forces'],
+        forces,
+        atol=1e-4
+    )
+
+    with npt.assert_raises(AssertionError):
+        npt.assert_allclose(target_predictions['energy'], np.zeros_like(energy))
+
+    with npt.assert_raises(AssertionError):
+        npt.assert_allclose(target_predictions['forces'], np.zeros_like(forces))
+
+
+@pytest.mark.parametrize('name', ['atat', 'dha', 'bb'])
+def test_molecules_free(name: str):
+    jax.config.update('jax_enable_x64', True)
+
+    package_dir = pathlib.Path(__file__).parent.parent.resolve()
+
+    # targets
+    target_predictions = np.load(package_dir / f'tests/test_data/{name}_jaxmd.npz')
+
+    # read molecule
+    mol = read(
+        package_dir / f'tests/test_data/{name}.xyz'
+    )
+
+    species = jnp.array(mol.get_atomic_numbers())
+    box = None
+    R0 = jnp.array(mol.get_positions())
+
+    # define mlff potential
+    mlff_potential = MLFFPotentialSparse.create_from_ckpt_dir(
+        package_dir / 'sup_gems' / 'sup_gems_params',
+        from_file=True,
+        long_range_kwargs=dict(
+            cutoff_lr=lr_cutoff,  # this controls whether elec and disp modules use force shifting and damping.
+            dispersion_energy_cutoff_lr_damping=lr_cutoff_damp,
+            neighborlist_format_lr='ordered_sparse'
+        ),
+        dtype=jnp.float64
+    )
+
+    displacement, shift = space.free()
+
+    neighbor_fn, neighbor_fn_lr, energy_fn = jaxmd_utils.to_jax_md(
+        potential=mlff_potential,
+        displacement_or_metric=displacement,
+        box_size=box,
+        species=species,
+        capacity_multiplier=capacity_multiplier,
+        buffer_size_multiplier_sr=buffer_size_multiplier_sr,
+        buffer_size_multiplier_lr=buffer_size_multiplier_lr,
+        minimum_cell_size_multiplier_sr=minimum_cell_size_multiplier_sr,
+        disable_cell_list=True
+    )
+
+    energy_fn = jax.jit(energy_fn)
+    force_fn = jax.jit(quantity.force(energy_fn))
+
+    nbrs = neighbor_fn.allocate(R0, box=box)
+    nbrs_lr = neighbor_fn_lr.allocate(R0, box=box)
+
+    energy = energy_fn(R0, neighbor=nbrs.idx, neighbor_lr=nbrs_lr.idx, box=box)
+    forces = force_fn(R0, neighbor=nbrs.idx, neighbor_lr=nbrs_lr.idx, box=box)
+
+    npt.assert_allclose(
+        target_predictions['energy'],
+        energy,
+        atol=1e-6
+    )
+
+    npt.assert_allclose(
+        target_predictions['forces'],
+        forces,
+        atol=1e-4
+    )
+
+    with npt.assert_raises(AssertionError):
+        npt.assert_allclose(target_predictions['energy'], np.zeros_like(energy))
+
+    with npt.assert_raises(AssertionError):
+        npt.assert_allclose(target_predictions['forces'], np.zeros_like(forces))
+
+
+def test_water():
+    jax.config.update('jax_enable_x64', True)
+
+    package_dir = pathlib.Path(__file__).parent.parent.resolve()
+
+    # targets
+    target_predictions = np.load(package_dir / f'tests/test_data/water_jaxmd.npz')
+
+    # read molecule
+    mol = read(
+        package_dir / f'tests/test_data/water_64.xyz'
+    )
+    mol.wrap()
+
+    # Repeat the cell to match reference predictions.
+    mol = mol.repeat([2, 2, 2])
+
+    species = jnp.array(mol.get_atomic_numbers())
+    atomic_masses = jnp.array(mol.get_masses())
+    box = jnp.array(jnp.diag(mol.get_cell().T))
+    R0 = jnp.array(mol.get_positions()) / box  # box can have different side lengths
+
+    # define mlff potential
+    mlff_potential = MLFFPotentialSparse.create_from_ckpt_dir(
+        package_dir / 'sup_gems' / 'sup_gems_params',
+        long_range_kwargs=dict(
+            cutoff_lr=lr_cutoff,  # this controls whether elec and disp modules use force shifting and damping.
+            dispersion_energy_cutoff_lr_damping=lr_cutoff_damp,
+            neighborlist_format_lr='ordered_sparse'
+        ),
+        dtype=jnp.float64
+        # determines the output dtype. When float64 is passed all calculations are performed in float64 and
+        # final result is casted to dtype.
+    )
+
+    displacement, shift = space.periodic_general(
+        box,
+        fractional_coordinates=True
+    )
+
+    neighbor_fn, neighbor_fn_lr, energy_fn = jaxmd_utils.to_jax_md(
+        potential=mlff_potential,
+        displacement_or_metric=displacement,
+        box_size=box,
+        species=species,
+        capacity_multiplier=capacity_multiplier,
+        buffer_size_multiplier_sr=buffer_size_multiplier_sr,
+        buffer_size_multiplier_lr=buffer_size_multiplier_lr,
+        minimum_cell_size_multiplier_sr=minimum_cell_size_multiplier_sr
+    )
+
+    energy_fn = jax.jit(energy_fn)
+    force_fn = jax.jit(quantity.force(energy_fn))
+
+    nbrs = neighbor_fn.allocate(R0, box=box)
+    nbrs_lr = neighbor_fn_lr.allocate(R0, box=box)
+
+    energy = energy_fn(R0, neighbor=nbrs.idx, neighbor_lr=nbrs_lr.idx, box=box)
+    forces = force_fn(R0, neighbor=nbrs.idx, neighbor_lr=nbrs_lr.idx, box=box)
+
+    npt.assert_allclose(
+        target_predictions['energy'],
+        energy,
+        atol=1e-6,
+    )
+
+    npt.assert_allclose(
+        target_predictions['forces'],
+        forces,
+        atol=5e-4
+    )
+
+    with npt.assert_raises(AssertionError):
+        npt.assert_allclose(target_predictions['energy'], np.zeros_like(energy))
+
+    with npt.assert_raises(AssertionError):
+        npt.assert_allclose(target_predictions['forces'], np.zeros_like(forces))
