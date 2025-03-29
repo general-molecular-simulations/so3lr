21f3e8a5e544eb0d23bb04b2a6e67a5811207d27
MeluXina user <u101941@login02.meluxina.lxp.lu>
2025-03-29 16:04:04 +0100

    enable parsing model directory

--- so3lr/potential.py
+++ so3lr/potential.py
@@ -7,12 +7,15 @@ from mlff.mdx.potential import MLFFPotentialSparse
 def make_potential_fn(
     lr_cutoff=12.,
         dispersion_energy_lr_cutoff_damping=2.,
-        dtype=np.float32
+        dtype=np.float32,
+        workdir= None
 ):
-    package_dir = pathlib.Path(__file__).parent.parent.resolve()
+    if workdir == None:
+        package_dir = pathlib.Path(__file__).parent.parent.resolve()
+        workdir=package_dir / 'so3lr' / 'params'
 
     return MLFFPotentialSparse.create_from_workdir(
-        workdir=package_dir / 'so3lr' / 'params',
+        workdir=workdir,
         long_range_kwargs=dict(
             cutoff_lr=lr_cutoff,
             dispersion_energy_cutoff_lr_damping=dispersion_energy_lr_cutoff_damping,
