c045581c75138eaae62eb168a28ea89806a04295
Adil <adil.kabylda@uni.lu>
2026-02-13 15:06:53 +0100

    print to install tensorflow_datasets only if it is used

--- so3lr/mlff/data/__init__.py
+++ so3lr/mlff/data/__init__.py
@@ -4,5 +4,9 @@ from .preprocessing import get_per_atom_shift
 from .dataloader_sparse_ase import AseDataLoaderSparse
 from .dataloader_sparse_npz import NpzDataLoaderSparse
 from .dataloader_sparse_spice import SpiceDataLoaderSparse
-from .dataloader_sparse_tfds import QCMLDataLoaderSparseParallel
 from . import transformations
+def __getattr__(name):
+    if name == "QCMLDataLoaderSparseParallel":
+        from .dataloader_sparse_tfds import QCMLDataLoaderSparseParallel
+        return QCMLDataLoaderSparseParallel
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
